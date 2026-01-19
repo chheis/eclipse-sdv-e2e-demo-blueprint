cde3edae0a0eedd4bdffe0349a90fe74a1309ba4
Christian Heissenberger <christian.heissenberger@eclipse-foundation.org>
2026-01-19 17:51:44 +0100

    ignoring arduino_secrets.h and arduino libraries

--- devices/driver-input-ecu-arduino/mcu2-joystick-input/mcu2-joystick-input.ino
+++ devices/driver-input-ecu-arduino/mcu2-joystick-input/mcu2-joystick-input.ino
@@ -1,3 +1,22 @@
+#include <WiFiS3.h>
+#include <ArduinoMqttClient.h>
+#include "arduino_secrets.h"
+
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;  // the WiFi radio's status
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "broker.hivemq.com";
+int port = 1883;
+const char topic[] = "SunFounder MQTT Test";
+
+//init buttons & states
+const int buttonPins[4] = { 2, 3, 4, 5 };
+bool previousButtonStates[4] = { false, false, false, false };
+
 const int xPin = A0;  //the VRX attach to
 const int yPin = A1;  //the VRY attach to
 const int swPin = 8;  //the SW attach to
